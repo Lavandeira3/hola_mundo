532d1081ede53d22679517ad9d057feb568452f5
manuel l suarez <mlavandeirasuarez@gmail.com>
2025-03-22 20:11:37 +0100

    add init base code

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <p className="text-2xl font-bold">Hola Mundo</p>
+    </div>
+  )
+}
+
+export default App
